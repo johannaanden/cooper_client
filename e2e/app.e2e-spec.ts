fad8ca4f8bc1d1a8c3c19bec36576a299b9f0699
johannaanden <johannaanden@hotmail.com>
2018-12-12 07:04:38 +0100

    Creates acceptance test for 'fill in form'

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -17,5 +17,11 @@ describe('App', () => {
                 expect(title).toEqual('Ionic App');
             });
         });
+
+        it('fill in form', () => {
+            page.fillInForm(1000, 'Female', 20);
+            expect(page.results_card_header()).toContain('Cooper Test Result');
+            expect(page.results_card_content()).toContain('Gender: female, Age: 20 Result: Poor');
+        });
     })
 });
