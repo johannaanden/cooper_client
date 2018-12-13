390d1c29e588ac8b6fcea9bc344d46952d135fc7
johannaanden <johannaanden@hotmail.com>
2018-12-13 14:36:13 +0100

    Updates calculate function to save the data to the backend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PersonProvider } from '../../providers/person/person';
+import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
 
 @Component({
   selector: 'page-home',
@@ -11,15 +12,19 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public person: PersonProvider) {
+    public person: PersonProvider,
+    public performanceData: PerformanceDataProvider) {
       this.user = { distance: 1000, age: 20, gender: 'female' };
     }
 
-  calculate() {
-    this.person.age = this.user.age;
-    this.person.gender = this.user.gender;
-    this.person.doAssessment(this.user.distance);
-    console.log(this.person.assessmentMessage)
+  calculate(user) {
+    debugger;
+    this.person.age = user.age;
+    this.person.gender = user.gender;
+    this.person.doAssessment(user.distance);
+    this.performanceData
+      .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
+      .subscribe(data => console.log(data));
   }
 
 }
