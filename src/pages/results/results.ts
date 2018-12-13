51c6d3846b51e48668dd6fa8c4192a832c64d5c9
johannaanden <johannaanden@hotmail.com>
2018-12-13 15:51:19 +0100

    Uppdated results.ts to fetch all results from backend when result page is loaded, adds card to show the result on results.html

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
 
 @Component({
   selector: 'page-results',
   templateUrl: 'results.html',
 })
 export class ResultPage {
+  result = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    private performanceData: PerformanceDataProvider,
+    public navCtrl: NavController, 
+    public navParams: NavParams
+    ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ResultsPage');
+    this.performanceData
+      .getResults()
+      .subscribe(data => (this.result = data.entries));
   }
 
 }
